04f18ea4a7680b796f1e6afb25998dcce19ebec1
micsoni <micsoni@gmail.com>
2020-03-26 11:39:58 +0100

    added AuthForm Component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import React from "react";
-
+import { Provider } from "react-redux";
+import { Route, Switch } from "react-router-dom";
+import store from "./store/index";
+import SignupFormContainer from "./components/SignUpFormContainer";
 function App() {
-  return <div>"hello"</div>;
+  return (
+    <Provider store={store}>
+      <p>hello</p>
+      <Switch>
+        <Route path="/signup" component={SignupFormContainer} />
+      </Switch>
+    </Provider>
+  );
 }
 
 export default App;
---
+++ src/components/AuthForm.js
@@ -0,0 +1,51 @@
+import React from "react";
+
+export default function AuthForm(props) {
+  
+  const typeOfForm = () => {
+    if (props.type === "Sign Up") {
+      return (
+        <div className="form-group col-12">
+          <label className="col-sm-2">Name</label>
+          <input 
+            type="text"
+            name="username"
+            onChange={props.onChange}
+            value={props.values.username}
+          />
+        </div>
+      );
+    } else {
+      return null;
+    }
+  };
+
+  return (
+    <div className="card text-center">
+      <form onSubmit={props.onSubmit}>
+        {typeOfForm()}
+        <div className="form-group col-12">
+          <label className="col-sm-2">Email</label>
+          <input 
+            type="email"
+            name="email"
+            onChange={props.onChange}
+            value={props.values.email}
+          />
+        </div>
+        <div className="form-group col-12">
+          <label className="col-sm-2">Password</label>
+          <input 
+            type="text"
+            name="password"
+            onChange={props.onChange}
+            value={props.values.password}
+          />
+        </div>
+        <button type="submit" className="btn btn-info">
+          {props.type}
+        </button>
+      </form>
+    </div>
+  );
+}
