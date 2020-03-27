4d2729852c0fcf98c5ba3ee1f92961fe4d0c307b
micsoni <micsoni@gmail.com>
2020-03-27 15:20:32 +0100

    added image to homepage

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,10 +1,15 @@
-import React from 'react'
-import CreatePlantFormContainer from "./CreatePlantFormContainer"
+import React from "react";
+import CreatePlantFormContainer from "./CreatePlantFormContainer";
+import "../style/Homepage.css";
 
 export default function Homepage() {
   return (
-    <div>
-     <CreatePlantFormContainer />
+    <div className="text-center">
+      <img
+        src="https://res.cloudinary.com/plants-health/image/upload/v1585306327/homepage_uw2qvo.png"
+        className="img-fluid"
+        alt=""
+      />
     </div>
-  )
+  );
 }
