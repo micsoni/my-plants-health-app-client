e0eaf98f78e1db9d98930565d37c693e72714c9e
micsoni <micsoni@gmail.com>
2020-03-31 22:22:36 +0200

    Created components for alarms

--- src/components/PlantDetailsPage.js
+++ src/components/PlantDetailsPage.js
@@ -3,58 +3,67 @@ import { connect } from "react-redux";
 import { Redirect } from "react-router-dom";
 import AlarmFormContainer from "./AlarmFormContainer";
 import usePushNotifications from "./Notification/useNotifications";
-
-//import {getCurrentPlant} from "../store/actions/plants"
+import {getCurrentPlant} from "../store/actions/plants"
+import UserAlarmsCardsList from "./UserAlarmsCardsList"
+import PlantDetails from "./PlantDetails"
 
 function PlantDetailsPage(props) {
-  const [toggleAlarmForm, setToggleForm] = useState(false);
+  const [toggleAlarmForm, setToggleAlarmForm] = useState(false);
 
+  const toggleAForm = () => {
+    setToggleAlarmForm(!toggleAlarmForm);
+  };
   const {
     onClickAskUserPermissionAndSubscribe,
   } = usePushNotifications();
 
-  const toggleForm = () => {
-    setToggleForm(!toggleAlarmForm);
-  };
+
+  const {getCurrentPlant} = props
+  const plantId = props.match.params.plantId
+  const currentUser = props.userLoggedIn.jwt
 
   useEffect(() => {
-    if (props.userLoggedIn.jwt) {
-      //props.getCurrentPlant(props.match.params.plantId);
+    if (currentUser) {
+     getCurrentPlant(plantId);
     }
-  });
+  }, [getCurrentPlant, plantId, currentUser]);
 
-  // const checkforAlarms = () => {
-  //   if (props.plants.alarms.length === 0) {
-  //     return <p>You haven't added any alarms yet</p>;
-  //   }
-  //   return (null)
-  //    <AlarmsList />
+  const checkforAlarms = () => {
+    if (props.plant.alarms.length === 0) {
+      return <p>You haven't added any alarms yet</p>;
+    }
+    return <UserAlarmsCardsList />
 
-  // };
+  };
 
   if (!props.userLoggedIn.jwt) {
     return <Redirect to="/login" />;
   }
 
-  if (!props.plant) {
+  if (!props.plant.id) {
     return <p>Loading...</p>;
   }
   return (
     <div className="container-fluid">
       <div className="row">
-        <div className="col-sm-12">
+        <div className="col-md-4 col-sm-12">
+          <PlantDetails plant={props.plant}/>
+          </div>
+          <div className="col-md-4 col-sm-12">
            <button
             className="btn"
             onClick={() => {
-              onClickAskUserPermissionAndSubscribe();
-              toggleForm();
+              onClickAskUserPermissionAndSubscribe(props.userLoggedIn);
+              toggleAForm();
             }}
-          >
-            {" "}
+          >{" "}
             New Alarm
           </button>       
           {toggleAlarmForm && <AlarmFormContainer />}
-          {/* {checkforAlarms()} */}
+          {checkforAlarms()}
+        </div>
+        <div className="col-md-4 col-sm-12">
+          hello
         </div>
       </div>
     </div>
@@ -67,6 +76,8 @@ function mapStateToProps(state) {
     plant: state.plants.current
   };
 }
-const mapDispatchToProps = {};
+const mapDispatchToProps = {
+  getCurrentPlant
+};
 
 export default connect(mapStateToProps, mapDispatchToProps)(PlantDetailsPage);
