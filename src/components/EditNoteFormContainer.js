c2046da6a40ee09937bbee49cea8824f8e5e649c
micsoni <micsoni@gmail.com>
2020-04-02 14:45:53 +0200

    added form to edit notes

---
+++ src/components/EditNoteFormContainer.js
@@ -0,0 +1,43 @@
+import React, { useState } from "react";
+import {connect} from "react-redux"
+import NoteForm from "./NoteForm";
+import {editNote} from "../store/actions/notes"
+import "../style/Forms.css";
+
+function EditNoteFormContainer(props) {
+  
+  const [note, setNote] = useState({
+ title:props.note.title,
+  text: props.note.text
+  });
+
+  const onSubmit = event => {
+    event.preventDefault();
+    props.editNote(props.plant.id, props.note.id, {title:note.title, text:note.text})
+  };
+
+  const onChange = event => {
+    console.log(event.target.name, event.target.value)
+    setNote({
+      ...note,
+      [event.target.name]: event.target.value
+    });
+  };
+
+  return (
+    <div className="form ">
+      <div className="card shadow-sm">
+        <p className="text-center">Edit information </p>
+        <NoteForm
+          onSubmit={onSubmit}
+          onChange={onChange}
+          values={note}
+          button={"Update Note"}
+        />
+      </div>
+    </div>
+  );
+}
+
+export default connect(null, { editNote })(EditNoteFormContainer);
+
