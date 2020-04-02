81c66be641fa822a379f8749d09bef00347cc464
micsoni <micsoni@gmail.com>
2020-04-02 14:46:29 +0200

    added all notes page

---
+++ src/components/NoteCardsList.js
@@ -0,0 +1,36 @@
+import React, {useState} from "react";
+import {connect} from "react-redux"
+import NoteCard from "./NoteCard"
+import EditNoteFormContainer from "./EditNoteFormContainer"
+
+function NoteCardsList(props) {
+  const [notesInEdit, setNotesInEdit] = useState([]);
+
+  const toggleNoteForm = note => {
+    const newState = notesInEdit.includes(note.id)
+      ? notesInEdit.filter(id => id !== note.id)
+      : notesInEdit.concat(note.id);
+
+    setNotesInEdit(newState);
+  };
+
+  return props.plant.notes.map(note => {
+    
+    const showForm = notesInEdit.includes(note.id);
+    return (
+      <div className="card" key={note.id}>
+        <NoteCard type={"details page"} plant={props.plant} note={note} toggleNoteForm={() => toggleNoteForm(note)} />
+        <div>{showForm && <EditNoteFormContainer plant={props.plant} note={note} />}</div>
+      </div>
+    );
+  });
+
+}
+
+function mapStateToProps(state) {
+  return {
+    plant: state.plants.current
+  };
+}
+
+export default connect(mapStateToProps)(NoteCardsList);
